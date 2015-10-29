247f6412dec9c30d03a77377ca101d7f5436785f
ashwinka <ashwinka@INashwinka.local>
2015-10-29 11:38:21 +0530

    make a working server file for the home page

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var http = require('http');
 var fs = require("fs");
-var showPage = function(){
-	var file = fs.readFileSync("swamijisWork.html","UTF-8");
-	return file;
-};
-
-var requestListener = function (req,res){
-	res.end(showPage());
-};
+var requestListener = function(req,res){
+	console.log(req.url);
+	if(req.url.match(/[.html][.css][.js]/))
+		res.end(fs.readFileSync('.'+req.url,"UTF-8"));
+	res.end(fs.readFileSync('.'+req.url));
+}
 var server = http.createServer(requestListener);
 server.listen(4000);
