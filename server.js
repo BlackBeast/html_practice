9c71eb8ad1d546508eb266d94c5e4bacfe82246e
ashwinka <ashwinka@INashwinka.local>
2015-10-29 11:51:05 +0530

    wrking server file for all pages

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require("fs");
 var requestListener = function(req,res){
 	console.log(req.url);
-	if(req.url.match(/[.html][.css][.js]/))
-		res.end(fs.readFileSync('.'+req.url,"UTF-8"));
+	// if(req.url.match(/[.html][.css][.js]/))
+	// 	res.end(fs.readFileSync('.'+req.url,"UTF-8"));
 	res.end(fs.readFileSync('.'+req.url));
 }
 var server = http.createServer(requestListener);
